8f13045b108069d497132b09301f250fa9aa3f07
Luis Alberto Acuna Fernandez <luisalacfe@gmail.com>
2022-02-25 12:37:45 +0100

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoAppService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
